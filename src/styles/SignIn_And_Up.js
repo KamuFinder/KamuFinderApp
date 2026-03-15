c17c8b64fe94b6cf312ef04fab15f4664f35134b
MrReaa <ronja.e.a.ahonen@gmail.com>
2026-03-16 01:25:58 +0200

    stylesheets

---
+++ src/styles/SignIn_And_Up.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#2196F3",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  link: {
+    color: "blue",
+  },
+  input: {
+    backgroundColor: "#b1b1b1",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+
+  },
+});

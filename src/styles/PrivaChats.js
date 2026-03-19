661ea1c9c8795dbcc3150e7964b0318b19c391d0
MrReaa <ronja.e.a.ahonen@gmail.com>
2026-03-20 00:32:32 +0200

    Yksityisviestit suurinpiirtein bäkkäri valmis

---
+++ src/styles/PrivaChats.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#fff",
+    },
+    chatItem: {
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 10,
+        backgroundColor: "#f5f5f5",
+    },
+    unread: {
+        backgroundColor: "#cce5ff",
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    message: {
+        marginTop: 5,
+        color: "#555",
+    },
+});

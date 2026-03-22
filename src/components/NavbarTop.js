7332a2e3abfb6dcaf9451dc37b94b1e4ef05d462
Jenni <t2vaje00@students.oamk.fi>
2026-03-22 22:49:47 +0200

    home screen Ui

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -5,24 +5,34 @@ import { useNavigation } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 
-export default function NavbarTop({ showBack = false, showProfile = false }) {
+export default function NavbarTop({ showBack = false, showProfile = false, showNotifications = false  }) {
     const navigation = useNavigation();
     const fit = useSafeAreaInsets();
   
 
   return (
     <View style={[styles.container, {paddingTop: fit.top +15 }]}>
-      {showBack && (
+      
+      <View style={{width:40}}>
+
+      {showBack ? (
       <TouchableOpacity onPress={() =>  navigation.goBack()}>
         <Ionicons name="arrow-back" size={30} />
       </TouchableOpacity>
-      )}
+      ) : showNotifications ? (
+        <TouchableOpacity onPress={() => navigation.navigate("Notifications")}
+        >
+          <Ionicons name="notifications-circle-outline" size={40} />
+        </TouchableOpacity>
+      ) : null}
+      </View>
+
 
       <View style={{flex:1}}/>
 
       {showProfile && (
         <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-          <Ionicons name="person-circle-outline" size={50} />
+          <Ionicons name="person-circle-outline" size={40} />
         </TouchableOpacity>
       )}
       

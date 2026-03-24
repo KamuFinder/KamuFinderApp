3b4bf2e00f1cfa4ac823c3e11ce6fceaecdad035
Anni <t9jyan00@students.oamk.fi>
2026-03-24 12:34:37 +0200

    AI

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from "react";
 import AppNavigator from "./src/navigation/Navigation.js";
 import { AuthProvider } from "./src/context/UserContext.js"
 
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+console.log("API KEY:", API_KEY);
+
 export default function App() {
   return (
     <AuthProvider >

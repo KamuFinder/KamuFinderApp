a8fcc152ae00043c47f18b39e06b52a4e7b048e6
MrReaa <ronja.e.a.ahonen@gmail.com>
2026-04-08 15:40:03 +0300

    Lista optionseista

---
+++ src/components/Options.js
@@ -0,0 +1,39 @@
+export const Options =  {
+    
+    hobbyOptions: [
+    "liikunta ja hyvinvointi",
+    "ulkoilu ja luonto",
+    "pelaaminen",
+    "musiikki",
+    "taide",
+    "valokuvaus ja video",
+    "ruoanlaitto ja leivonta",
+    "lukeminen ja kirjoittaminen",
+    "käsityöt",
+    "teknologia ja elektroniikka",
+    "autot ja moottorit",
+    "matkustaminen",
+    "eläimet ja lemmikit",
+    "kulttuuri ja tapahtumat",
+    "sosiaalinen tekeminen"
+
+    ],
+    studyOptions: [    
+    "teknologia ja tietojenkäsittely",
+    "kauppatiede ja liiketalous",
+    "opetus ja kasvatus",
+    "psykologia",
+    "terveys ja lääketiede",
+    "luonnontieteet",
+    "matematiikka",
+    "kielet ja viestintä",
+    "ympäristö",
+    "muotoilu",
+    "oikeustiede",
+    "taide ja kulttuuri",
+    "sosiaali ja yhteiskunta",
+    "matkailu ja ravitsemisala",
+    "urheilu ja liikunta"
+
+    ]
+}
